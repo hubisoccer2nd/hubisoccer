39bd5532bf70eeedb18f79fe02c4b93edd11fe6d
hubisoccer2nd <thehubisoccer@gmail.com>
2026-04-13 22:49:57 +0100

    Update utils.js

--- hubisapp/shared/community/utils.js
+++ hubisapp/shared/community/utils.js
@@ -90,10 +90,10 @@ function escapeHtml(str) {
     if (!str) return '';
     const map = {
         '&': '&amp;',
-        '<': '&lt;',
-        '>': '&gt;',
-        '"': '&quot;',
-        "'": '&#39;'
+        //'<': '&lt;',
+        //'>': '&gt;',
+        //'"': '&quot;',
+        //"'": '&#39;'
     };
     return String(str).replace(/[&<>"']/g, c => map[c]);
 }

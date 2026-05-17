1c8f84c31085de0cce83a948194f32ce976fede8
hubisoccer2nd <thehubisoccer@gmail.com>
2026-05-17 12:33:26 +0100

    Update premier-pas-login.js

--- premier-pas-login/premier-pas-login.js
+++ premier-pas-login/premier-pas-login.js
@@ -1,4 +1,4 @@
-t// ========== PREMIER-PAS-LOGIN.JS ==========
+// ========== PREMIER-PAS-LOGIN.JS ==========
 // ========== DÉBUT : CONFIGURATION SUPABASE ==========
 const SUPABASE_URL = 'https://rasepmelflfjtliflyrz.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJhc2VwbWVsZmxmanRsaWZseXJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyOTA0MDEsImV4cCI6MjA4OTg2NjQwMX0.5_aw5JMVeIB8BePdZylI7gGN7pCD79CkS2AResneVpY';
